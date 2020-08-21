875654905a62759ea6afec1ca0c7b1bdd04b16ff
romanstukalkin <stukalkinrv@yandex.ru>
2020-08-21 13:40:48 +0400

    it's done, but...

--- dropboxserver/src/main/java/MainHandler.java
+++ dropboxserver/src/main/java/MainHandler.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class MainHandler extends ChannelInboundHandlerAdapter {
     final String serverRootPath = "./ServerDir";
-    HashMap <String, String> loginPassword = new HashMap <String, String>();
+    static HashMap <String, String> loginPassword = new HashMap <String, String>();
 
     @Override
     public void channelActive(ChannelHandlerContext channelHandlerContext){
@@ -37,19 +37,22 @@ public class MainHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws IOException {
-            CommandMessage cm = (CommandMessage) msg; // прилетело сообщение
-            if (cm.getParametr() == CommandMessage.Parametr.File) { // если прилетел файл
-                File file = new File(serverRootPath + "/" + cm.getFilename()); //создаем файл
-                if (!file.exists()) { //если такой не существует
-                    Files.write(Paths.get(file.getPath()), cm.getBytes()); // то создаем файл и в него кидаем байты
-                } else System.out.println("File exists"); // если файл есть, то выйдет сообщение
-            } else if (cm.getParametr() == CommandMessage.Parametr.Info) { // но если прилетело инфо
-                File file = new File(serverRootPath + "/" + cm.getFilename()); // создаем файл с новым путем
-                if (!file.exists()) { // если не существует такой файл
-                    CommandMessage сmOut = new CommandMessage("File not exist"); //то посылаем сообщение клиенту
-                    ctx.writeAndFlush(сmOut); // вот тут посылаем
-                } else { //ну а если есть
-                    CommandMessage сmOut = new CommandMessage(Paths.get(file.getPath())); //то посылаем байты
+            CommandMessage cm = (CommandMessage) msg;
+            if (cm.getParametr() == CommandMessage.Parametr.File) {
+                File file = new File(serverRootPath + "/" + cm.getFilename());
+                if (!file.exists()) {
+                    Files.write(Paths.get(file.getPath()), cm.getBytes());
+                } else {
+                    CommandMessage сmOut = new CommandMessage("File not exist");
+                    ctx.writeAndFlush(сmOut);
+                }
+            } else if (cm.getParametr() == CommandMessage.Parametr.Info) {
+                File file = new File(serverRootPath + "/" + cm.getFilename());
+                if (!file.exists()) {
+                    CommandMessage сmOut = new CommandMessage("File not exist");
+                    ctx.writeAndFlush(сmOut);
+                } else {
+                    CommandMessage сmOut = new CommandMessage(Paths.get(file.getPath()));
                     ctx.writeAndFlush(сmOut);
                 }
             } else if (cm.getParametr() == CommandMessage.Parametr.Auth) {
